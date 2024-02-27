7a10973b4638f30f8aef8ea22855c6fcfa17675f
Ekaterina Petukhova <katepthv12@gmail.com>
2024-02-27 11:56:30 +0100

    feat: solve getTime

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -31,8 +31,15 @@ function dateToTimestamp(date) {
  * Date(2023, 5, 1, 8, 20, 55) => '08:20:55'
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
-function getTime(/* date */) {
-  throw new Error('Not implemented');
+function getTime(date) {
+  const hours = date.getHours() >= 10 ? date.getHours() : `0${date.getHours()}`;
+  const minutes =
+    date.getMinutes() >= 10 ? date.getMinutes() : `0${date.getMinutes()}`;
+  const seconds =
+    date.getSeconds() >= 10 ? date.getSeconds() : `0${date.getSeconds()}`;
+  const time = `${hours}:${minutes}:${seconds}`;
+
+  return time;
 }
 
 /**
