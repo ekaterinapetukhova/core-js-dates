da7b1fb28c987f8cd34827beca147673af0e3d6a
Ekaterina Petukhova <katepthv12@gmail.com>
2024-02-29 22:00:08 +0100

    feat: solve isLeapYear

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -316,8 +316,15 @@ function getWorkSchedule(/* period, countWorkDays, countOffDays */) {
  * Date(2022, 2, 1) => false
  * Date(2020, 2, 1) => true
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = date.getFullYear();
+  let isLeap = false;
+
+  if (year % 4 === 0) {
+    isLeap = true;
+  }
+
+  return isLeap;
 }
 
 module.exports = {
